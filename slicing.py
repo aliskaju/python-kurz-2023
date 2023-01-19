835988d50ebc003812ad9f4cf84cdc02db518319
Aliska Ju <karameli@seznam.cz>
2023-01-19 20:37:31 +0100

    zkouska

---
+++ slicing.py
@@ -0,0 +1,16 @@
+print("Dobry"+" "+"den")
+
+venecky = [1,2,4,1,6,0,1]
+print(venecky +[0,1,2,0,2,0,5])
+
+#Slicing
+jmeno = "Andy"
+print(jmeno[3])
+print(jmeno[-1]) # posledni prvek
+
+
+venecky = [1,2,4,1,6,0,1]
+
+print(venecky[0:5])
+
+print(venecky[1:6])
